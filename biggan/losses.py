d1bf62133b6c4cea699a291d4fa9e8b3b21753e2
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2019-10-14 21:39:50 +0900

    add binary cross entropy loss

--- biggan/losses.py
+++ biggan/losses.py
@@ -1,13 +1,28 @@
 import tensorflow as tf
 
 
-def get_d_real_loss(logits_real):
-    loss = tf.nn.relu(1.0 - logits_real)
-    return tf.reduce_mean(loss)
+def discriminator_hinge_loss(logits_real, logits_fake):
+    loss_real = tf.nn.relu(1.0 - logits_real)
+    loss_fake = tf.nn.relu(1.0 + logits_fake)
+    return tf.reduce_mean(loss_real + loss_fake)
 
-def get_d_fake_loss(logits_fake):
-    loss = tf.nn.relu(1.0 + logits_fake)
-    return tf.reduce_mean(loss)
 
-def get_g_loss(logits_fake):
+def generator_hinge_loss(logits_fake):
     return -tf.reduce_mean(logits_fake)
+
+
+def bce(y_true, y_pred, from_logits=True, label_smoothing=0):
+    loss = tf.losses.binary_crossentropy(y_true, y_pred,
+                                         from_logits, label_smoothing)
+    return loss
+
+
+def discriminator_bce_loss(logits_real, logits_fake):
+    loss_real = bce(tf.ones_like(logits_real), logits_real)
+    loss_fake = bce(tf.zeros_like(logits_fake), logits_fake)
+    return tf.reduce_mean(loss_real + loss_fake)
+
+
+def generator_bce_loss(logits_fake):
+    loss = bce(tf.ones_like(logits_fake), logits_fake)
+    return tf.reduce_mean(loss)
