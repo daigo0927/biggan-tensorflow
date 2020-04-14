b131299a75aaa1b09b9d115344f5708a1494e5c2
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2020-04-15 04:35:02 +0900

    add use_bias option #3 #4

--- biggan/ops.py
+++ biggan/ops.py
@@ -18,10 +18,10 @@ from tensorflow.keras import layers, initializers
 # limitations under the License.
 
 
-def SpectralNormalization(layers.Layer):
+class SpectralNormalization(layers.Layer):
     def __init__(self,
                  n_power_iters=1,
-                 initializer=initializers.RandomNomral(),
+                 initializer=initializers.RandomNormal(),
                  **kwargs):
         super().__init__(**kwargs)
         self.n_power_iters = n_power_iters
@@ -33,16 +33,17 @@ def SpectralNormalization(layers.Layer):
         self.u = self.add_weight('u',
                                  shape=[w.shape[0], 1],
                                  dtype=tf.float32,
-                                 initializers=self.initializer,
+                                 initializer=self.initializer,
                                  trainable=False)
 
     def call(self, inputs, training=None):
-        w_shape = inputs.get_shape() # Conv2D case: (k, k, ch1, ch2)
-        w = tf.reshpae(inputs, [-1, w_shape[-1]]) # (kxkxch1, ch2)
-        
-        u = self.u # (kxkxch1, 1)
+        w_shape = inputs.get_shape()  # Conv2D case: (k, k, ch1, ch2)
+        w = tf.reshape(inputs, [-1, w_shape[-1]])  # (kxkxch1, ch2)
+
+        u = self.u  # (kxkxch1, 1)
         for _ in range(num_iters):
-            v = tf.nn.l2_normalize(tf.matmul(w, u, transpose_a=True))  # (ch2, 1)
+            v = tf.nn.l2_normalize(tf.matmul(w, u,
+                                             transpose_a=True))  # (ch2, 1)
             u = tf.nn.l2_normalize(tf.matmul(w, v))  # (kxkxch1, 1)
 
         if training:
@@ -64,10 +65,11 @@ class SNConv2D(layers.Layer):
                  filters,
                  kernel_size=(3, 3),
                  strides=(2, 2),
+                 use_bias=True,
                  sn_iters=1,
                  kernel_initializer=initializers.Orthogonal(),
                  bias_initializer=initializers.Zeros(),
-                 u_initializer=initializers.RandomNomral(),
+                 u_initializer=initializers.RandomNormal(),
                  **kwargs):
         super().__init__(**kwargs)
         self.filters = filters
@@ -75,6 +77,7 @@ class SNConv2D(layers.Layer):
         self.kernel_size = kernel_size
         assert len(strides) == 2, 'strides must be 2 length'
         self.strides = strides
+        self.use_bias = use_bias
         self.sn_iters = sn_iters
         self.kernel_initializer = kernel_initializer
         self.bias_initializer = bias_initializer
@@ -87,9 +90,10 @@ class SNConv2D(layers.Layer):
         self.kernel = self.add_weight('kernel',
                                       shape=kernel_shape,
                                       initializer=self.kernel_initializer)
-        self.bias = self.add_weight('bias',
-                                    shape=[self.filters],
-                                    initializer=self.bias_initializer)
+        if self.use_bias:
+            self.bias = self.add_weight('bias',
+                                        shape=[self.filters],
+                                        initializer=self.bias_initializer)
         self.sn = SpectralNormalization(n_power_iters=self.sn_iters,
                                         initializer=self.u_initializer,
                                         name='spectral_normalization')
@@ -100,19 +104,22 @@ class SNConv2D(layers.Layer):
                          kernel,
                          strides=[1, *self.strides, 1],
                          padding='SAME')
-        x = tf.nn.bias_add(x, self.bias)
+        if self.use_bias:
+            x = tf.nn.bias_add(x, self.bias)
         return x
 
 
 def SNConv1x1(filters,
+              use_bias=True,
               sn_iters=1,
               kernel_initializer=initializers.Orthogonal(),
               bias_initializer=initializers.Zeros(),
-              u_initializer=initializers.RandomNomral(),
+              u_initializer=initializers.RandomNormal(),
               **kwargs):
     return SNConv2D(filters=filters,
                     kernel_size=(1, 1),
                     strides=(1, 1),
+                    use_bias=use_bias,
                     sn_iters=sn_iters,
                     kernel_initializer=kernel_initializer,
                     bias_initializer=bias_initializer,
@@ -123,13 +130,15 @@ def SNConv1x1(filters,
 class SNLinear(layers.Layer):
     def __init__(self,
                  units,
+                 use_bias=True,
                  sn_iters=1,
                  kernel_initializer=initializers.Orthogonal(),
                  bias_initializer=initializers.Zeros(),
-                 u_initializer=initializers.RandomNomral(),
+                 u_initializer=initializers.RandomNormal(),
                  **kwargs):
         super().__init__(**kwargs)
         self.units = units
+        self.use_bias = use_bias
         self.sn_iters = sn_iters
         self.kernel_initializer = kernel_initializer
         self.bias_initializer = bias_initializer
@@ -141,16 +150,19 @@ class SNLinear(layers.Layer):
         self.kernel = self.add_weight('kernel',
                                       shape=kernel_shape,
                                       initializer=self.kernel_initializer)
-        self.bias = self.add_weight('bias',
-                                    shape=[self.units],
-                                    initializer=self.bias_initializer)
+        if self.use_bias:
+            self.bias = self.add_weight('bias',
+                                        shape=[self.units],
+                                        initializer=self.bias_initializer)
         self.sn = SpectralNormalization(n_power_iters=self.sn_iters,
                                         initializer=self.u_initializer,
                                         name='spectral_normalization')
 
     def call(self, inputs, training=None):
         kernel = self.sn(self.kernel, training)
-        x = tf.matmul(inputs, kernel) + self.bias
+        x = tf.matmul(inputs, kernel)
+        if self.use_bias:
+            x += self.bias
         return x
 
 
@@ -160,7 +172,7 @@ class SNEmbedding(layers.Layer):
                  output_dim,
                  sn_iters=1,
                  embeddings_initializer=initializers.RandomUniform(),
-                 u_initializer=initializers.RandomNomral(),
+                 u_initializer=initializers.RandomNormal(),
                  **kwargs):
         super().__init__(**kwargs)
         self.input_dim = input_dim
@@ -170,9 +182,10 @@ class SNEmbedding(layers.Layer):
         self.u_initializer = u_initializer
 
     def build(self, input_shape):
-        self.embeddings = self.add_weight('embeddings',
-                                          shape=[self.input_dim, self.output_dim],
-                                          initializer=self.embeddings_initializer)
+        self.embeddings = self.add_weight(
+            'embeddings',
+            shape=[self.input_dim, self.output_dim],
+            initializer=self.embeddings_initializer)
         self.sn = SpectralNormalization(n_power_iters=self.sn_iters,
                                         initializer=self.u_initializer,
                                         name='spectral_normalization')
@@ -180,97 +193,134 @@ class SNEmbedding(layers.Layer):
     def call(self, inputs, training=None):
         embeddings_T = self.sn(tf.transpose(self.embeddings), training)
         embeddings = tf.transpose(embeddings_T)
-        x = tf.nn.embedding_lookup(embed_map_bar, inputs)
+        x = tf.nn.embedding_lookup(embeddings, inputs)
         return x
 
 
-class SelfAttention(layers.Layer):
+class SNSelfAttention(layers.Layer):
     def __init__(self,
-                 initializer=tf.initializers.orthogonal(),
-                 name='self_attention'):
-        super(SelfAttention, self).__init__(name=name)
-        self.initializer = initializer
+                 use_bias=False,
+                 sn_iters=1,
+                 kernel_initializer=initializers.Orthogonal(),
+                 bias_initializer=initializers.Zeros(),
+                 u_initializer=initializers.RandomNormal(),
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.use_bias = use_bias
+        self.sn_iters = sn_iters
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.u_initializer = u_initializer
+
+        self._params = {
+            'use_bias': use_bias,
+            'sn_iters': sn_iters,
+            'kernel_initializer': kernel_initializer,
+            'bias_initializer': bias_initializer,
+            'u_initializer': u_initializer
+        }
 
     def build(self, input_shape):
         in_channels = int(input_shape[-1])
         self.conv_theta = SNConv1x1(in_channels // 8,
-                                    initializer=self.initializer,
-                                    name='sn_conv_theta')
+                                    name='sn_conv_theta',
+                                    **self._params)
         self.conv_phi = SNConv1x1(in_channels // 8,
-                                  initializer=self.initializer,
-                                  name='sn_conv_phi')
+                                  name='sn_conv_phi',
+                                  **self._params)
         self.conv_g = SNConv1x1(in_channels // 2,
-                                initializer=self.initializer,
-                                name='sn_conv_g')
+                                name='sn_conv_g',
+                                **self._params)
         self.conv_attn = SNConv1x1(in_channels,
-                                   initializer=self.initializer,
-                                   name='sn_conv_attn')
+                                   name='sn_conv_attn',
+                                   **self._params)
         self.sigma = self.add_weight('sigma',
                                      shape=[],
                                      initializer=tf.zeros_initializer())
 
-    def call(self, x, training=None):
-        batch_size, h, w, in_channels = map(int, x.shape.as_list())
+    def call(self, inputs, training=None):
+        _, h, w, in_channels = inputs.shape.as_list()
         location_num = h * w
         downsampled_num = location_num // 4
 
-        theta = self.conv_theta(x, training=training)
-        theta = tf.reshape(theta, [batch_size, location_num, in_channels // 8])
+        theta = self.conv_theta(inputs, training)
+        theta = tf.reshape(theta, [-1, location_num, in_channels // 8])
 
-        phi = self.conv_phi(x, training=training)
+        phi = self.conv_phi(inputs, training)
         phi = tf.nn.max_pool(phi, ksize=[2, 2], strides=2, padding='VALID')
-        phi = tf.reshape(phi, [batch_size, downsampled_num, in_channels // 8])
+        phi = tf.reshape(phi, [-1, downsampled_num, in_channels // 8])
 
         attn = tf.matmul(theta, phi, transpose_b=True)
         attn = tf.nn.softmax(attn)
 
-        g = self.conv_g(x, training=training)
+        g = self.conv_g(inputs, training)
         g = tf.nn.max_pool(g, ksize=[2, 2], strides=2, padding='VALID')
-        g = tf.reshape(g, [batch_size, downsampled_num, in_channels // 2])
+        g = tf.reshape(g, [-1, downsampled_num, in_channels // 2])
 
         attn_g = tf.matmul(attn, g)
-        attn_g = tf.reshape(attn_g, [batch_size, h, w, in_channels // 2])
-        attn_g = self.conv_attn(attn_g, training=training)
+        attn_g = tf.reshape(attn_g, [-1, h, w, in_channels // 2])
+        attn_g = self.conv_attn(attn_g, training)
 
-        return x + self.sigma * attn_g
+        return inputs + self.sigma * attn_g
 
 
 class ConditionalBatchNorm(layers.Layer):
     def __init__(self,
-                 num_features,
                  axis=-1,
                  momentum=0.999,
                  epsilon=1E-5,
-                 initializer=tf.initializers.orthogonal(),
-                 name='conditional_batch_norm'):
-        super(ConditionalBatchNorm, self).__init__(name=name)
-        self.num_features = num_features
+                 use_bias=False,
+                 sn_iters=1,
+                 kernel_initializer=initializers.Orthogonal(),
+                 bias_initializer=initializers.Zeros(),
+                 u_initializer=initializers.RandomNormal(),
+                 **kwargs):
+        super().__init__(**kwargs)
         self.axis = axis
         self.momentum = momentum
         self.epsilon = epsilon
-        self.bn = tf.keras.layers.BatchNormalization(axis,
-                                                     momentum,
-                                                     epsilon,
-                                                     center=False,
-                                                     scale=False,
-                                                     name='bn')
-        self.linear_beta = SNLinear(num_features,
-                                    use_bias=False,
-                                    initializer=initializer,
-                                    name='sn_linear_beta')
-        self.linear_gamma = SNLinear(num_features,
-                                     use_bias=False,
-                                     initializer=initializer,
-                                     name='sn_linear_gamma')
+        self.use_bias = use_bias
+        self.sn_iters = sn_iters
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.u_initializer = u_initializer
+
+        self._params = {
+            'use_bias': use_bias,
+            'sn_iters': sn_iters,
+            'kernel_initializer': kernel_initializer,
+            'bias_initializer': bias_initializer,
+            'u_initializer': u_initializer
+        }
+
+        # TODO: implement use_bias argment, referred as https://github.com/huggingface/pytorch-pretrained-BigGAN/blob/master/pytorch_pretrained_biggan/model.py
+        # official TF implementation: https://github.com/tensorflow/tensorflow/blob/v2.1.0/tensorflow/python/keras/layers/convolutional.py#L48
+
+    def build(self, input_shape):
+        x_shape, condition_shape = input_shape
+        x_channels = x_shape[-1]
+
+        self.bn = layers.BatchNormalization(self.axis,
+                                            self.momentum,
+                                            self.epsilon,
+                                            center=False,
+                                            scale=False,
+                                            name='bn')
+        self.linear_beta = SNLinear(x_channels,
+                                    name='sn_linear_beta',
+                                    **self._params)
+        self.linear_gamma = SNLinear(x_channels,
+                                     name='sn_linear_gamma',
+                                     **self._params)
 
     def call(self, inputs, training=None):
         x, condition = inputs
-        beta = self.linear_beta(condition, training=training)
+        beta = self.linear_beta(condition, training)
         beta = tf.expand_dims(tf.expand_dims(beta, 1), 1)
-        gamma = self.linear_gamma(condition, training=training)
+        gamma = self.linear_gamma(condition, training)
         gamma = tf.expand_dims(tf.expand_dims(gamma, 1), 1)
 
-        x = self.bn(x, training=training)
+        x = self.bn(x, training)
         x = (1.0 + gamma) * x + beta
         return x
 
@@ -287,21 +337,19 @@ if __name__ == '__main__':
     embedding_size = 128
     num_iters = 1
 
-    sn = spectral_norm
-    conv2d = SNConv2d(filters)
+    sn = SpectralNormalization()
+    conv2d = SNConv2D(filters)
     conv1x1 = SNConv1x1(filters)
     linear = SNLinear(units)
     embed0 = layers.Embedding(num_classes, embedding_size)
     embed = SNEmbedding(num_classes, embedding_size)
-    self_attn = SelfAttention()
+    self_attn = SNSelfAttention()
     cbn = ConditionalBatchNorm(input_dim)
 
     # Create sample inputs
     weights = tf.random.normal((*kernel_size, input_dim, output_dim),
                                dtype=tf.float32)
     weights = tf.Variable(weights)
-    u = tf.random.normal((1, output_dim), dtype=tf.float32)
-    u = tf.Variable(u)
     images = tf.random.normal((batch_size, 64, 64, 3), dtype=tf.float32)
     features = tf.random.normal((batch_size, 100), dtype=tf.float32)
     labels = tf.random.uniform((batch_size, ),
@@ -315,11 +363,11 @@ if __name__ == '__main__':
     norm = np.linalg.norm
     import scipy as sp
     w_np = weights.numpy().reshape((-1, output_dim))
-    u_np, s_np, vt_np = sp.linalg.svd(w_np.T)
+    u_np, s_np, vt_np = sp.linalg.svd(w_np)
     u0_np = u_np[:, 0]
     for _ in range(100):
-        _ = sn(weights, u, num_iters, training)
-    u_pseudo = u.numpy()
+        _ = sn(weights, training)
+    u_pseudo = sn.u.numpy()[:, 0]
     cossim = np.abs(np.sum(u0_np * u_pseudo)) / norm(u0_np) / norm(u_pseudo)
     if cossim > 0.95:
         print(f'Singular vector similarity: {cossim}')
@@ -337,8 +385,8 @@ if __name__ == '__main__':
 
     training = False
     for _ in range(100):
-        _ = sn(weights, u, num_iters, training)
-    u_fix = u.numpy()
+        _ = sn(weights, training)
+    u_fix = sn.u.numpy()[:, 0]
     cossim = np.abs(np.sum(u_pseudo * u_fix)) / norm(u_pseudo) / norm(u_fix)
     if cossim > 0.95:
         print(f'Singular vector preservation : {cossim}')
