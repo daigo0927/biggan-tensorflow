fd3b9f1dc7ce853cd4a63f2ba94d30164a6629fb
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2020-04-20 23:43:27 +0900

    check architecture of BigGAN 64 resolution #6

--- biggan/biggan.py
+++ biggan/biggan.py
@@ -11,8 +11,8 @@ class GBlock(layers.Layer):
         self.upsample = upsample
 
     def build(self, input_shape):
-        self.bn_1 = ops.ConditionalBatchNorm(use_bias=False, name='bn_1')
-        self.bn_2 = ops.ConditionalBatchNorm(use_bias=False, name='bn_2')
+        self.bn_1 = ops.ConditionalBatchNorm(name='bn_1')
+        self.bn_2 = ops.ConditionalBatchNorm(name='bn_2')
 
         self.activation = layers.ReLU()
         if self.upsample:
@@ -38,7 +38,7 @@ class GBlock(layers.Layer):
 
 
 class Generator(tf.keras.Model):
-    def __init__(self, num_classes, base_dim=64, embedding_size=100, **kwargs):
+    def __init__(self, num_classes, base_dim=64, embedding_size=128, **kwargs):
         super().__init__(**kwargs)
         self.num_classes = num_classes
         self.base_dim = base_dim
@@ -48,11 +48,11 @@ class Generator(tf.keras.Model):
         self.embed = layers.Embedding(self.num_classes,
                                       self.embedding_size,
                                       name='embed')
-        self.linear = ops.SNLinear(self.base_dim * 16 * 4 * 4, name='linear')
+        self.linear = ops.SNLinear(self.base_dim * 16 * 4 * 4)
         self.block_1 = GBlock(self.base_dim * 16, name='block_1')
         self.block_2 = GBlock(self.base_dim * 8, name='block_2')
         self.block_3 = GBlock(self.base_dim * 4, name='block_3')
-        self.attn = ops.SNSelfAttention(use_bias=False, name='attn')
+        self.attn = ops.SNSelfAttention(name='attn')
         self.block_4 = GBlock(self.base_dim * 2, name='block_4')
 
         self.out = tf.keras.Sequential([
@@ -78,89 +78,6 @@ class Generator(tf.keras.Model):
         x = self.attn(x, training)
         x = self.block_4([x, conditions[3]], training)
         return self.out(x, training)
-
-
-class DCGenerator(tf.keras.Model):
-    """ Basic DCGAN (could have a few differences from the original one) """
-    def __init__(self, gf_dim=64, name='generator'):
-        super().__init__(name=name)
-        self.gf_dim = gf_dim
-
-        self.seq = tf.keras.Sequential([
-            layers.Dense(gf_dim * 8 * 4 * 4, use_bias=False, name='fc'),
-            layers.Reshape((4, 4, gf_dim * 8)),
-            layers.BatchNormalization(name='bn'),
-            layers.ReLU(),
-            layers.UpSampling2D(),
-            layers.Conv2D(gf_dim * 4, (5, 5), padding='same'),
-            layers.BatchNormalization(name='bn'),
-            layers.ReLU(),
-            layers.UpSampling2D(),
-            layers.Conv2D(gf_dim * 2, (5, 5), padding='same'),
-            layers.BatchNormalization(name='bn'),
-            layers.ReLU(),
-            layers.UpSampling2D(),
-            layers.Conv2D(gf_dim, (5, 5), padding='same'),
-            layers.BatchNormalization(name='bn'),
-            layers.ReLU(),
-            layers.UpSampling2D(),
-            layers.Conv2D(3, (5, 5), padding='same'),
-            layers.Activation('tanh')
-        ])
-
-    def call(self, inputs, training=None):
-        outputs = self.seq(inputs, training=training)
-        return outputs
-
-
-class ResNetGenerator(tf.keras.Model):
-    def __init__(self, gf_dim=64, name='generator'):
-        super(ResNetGenerator, self).__init__(name=name)
-        self.gf_dim = gf_dim
-
-        self.fc = layers.Dense(gf_dim * 8 * 4 * 4, name='fc')
-        self.block1 = self._block(gf_dim * 8, name='block_1')
-        self.sc1 = layers.Conv2D(gf_dim * 8, (1, 1), name='sc_1')
-        self.block2 = self._block(gf_dim * 4, name='block_2')
-        self.sc2 = layers.Conv2D(gf_dim * 4, (1, 1), name='sc_2')
-        self.block3 = self._block(gf_dim * 2, name='block_3')
-        self.sc3 = layers.Conv2D(gf_dim * 2, (1, 1), name='sc_3')
-        self.block4 = self._block(gf_dim, name='block_4')
-        self.sc4 = layers.Conv2D(gf_dim, (1, 1), name='sc_4')
-
-        self.bn = layers.BatchNormalization(momentum=0.9999,
-                                            epsilon=1e-5,
-                                            name='bn')
-        self.conv = layers.Conv2D(3, (3, 3), (1, 1), 'same', name='conv')
-
-    def call(self, x, training=None):
-        x = self.fc(x)
-        x = tf.reshape(x, [-1, 4, 4, self.gf_dim * 8])
-        fx = self.block1(x, training=training)
-        x = self.sc1(usample(x)) + fx
-        fx = self.block2(x, training=training)
-        x = self.sc2(usample(x)) + fx
-        fx = self.block3(x, training=training)
-        x = self.sc3(usample(x)) + fx
-        fx = self.block4(x, training=training)
-        x = self.sc4(usample(x)) + fx
-
-        x = tf.nn.relu(self.bn(x, training=training))
-        x = tf.nn.tanh(self.conv(x))
-        return x
-
-    def _block(self, filters, name):
-        seq = tf.keras.Sequential([
-            layers.BatchNormalization(name='bn_1'),
-            layers.ReLU(),
-            layers.UpSampling2D(),
-            layers.Conv2D(filters, (3, 3), (1, 1), 'same', name='conv_1'),
-            layers.BatchNormalization(name='bn_2'),
-            layers.ReLU(),
-            layers.Conv2D(filters, (3, 3), (1, 1), 'same', name='conv_2')
-        ],
-                                  name=name)
-        return seq
 
 
 class DBlock(layers.Layer):
@@ -238,77 +155,6 @@ class Discriminator(tf.keras.Model):
         return out
 
 
-class DCDiscriminator(tf.keras.Model):
-    """ https://www.tensorflow.org/tutorials/generative/dcgan """
-    def __init__(self, df_dim=64, name='discriminator'):
-        super().__init__(name=name)
-        self.df_dim = df_dim
-
-        self.seq = tf.keras.Sequential([
-            layers.Conv2D(df_dim, (5, 5), (2, 2), 'same'),
-            layers.LeakyReLU(),
-            layers.Conv2D(df_dim * 2, (5, 5), (2, 2), 'same'),
-            layers.LeakyReLU(),
-            layers.Conv2D(df_dim * 4, (5, 5), (2, 2), 'same'),
-            layers.LeakyReLU(),
-            layers.Flatten(),
-            layers.Dense(128),
-            layers.LeakyReLU(),
-            layers.Dropout(0.5),
-            layers.Dense(1)
-        ])
-
-    def call(self, inputs, training=None):
-        outputs = self.seq(inputs, training=training)
-        return outputs
-
-
-class ResNetDiscriminator(tf.keras.Model):
-    def __init__(self, df_dim=64, name='discriminator'):
-        super(ResNetDiscriminator, self).__init__(name=name)
-        self.df_dim = df_dim
-
-        self.block1 = self._block(df_dim, name='block_1')
-        self.sc1 = layers.Conv2D(df_dim, (1, 1), name='sc_1')
-        self.block2 = self._block(df_dim * 2, name='block_2')
-        self.sc2 = layers.Conv2D(df_dim * 2, (1, 1), name='sc_2')
-        self.block3 = self._block(df_dim * 4, name='block_3')
-        self.sc3 = layers.Conv2D(df_dim * 4, (1, 1), name='sc_3')
-        self.block4 = self._block(df_dim * 8, name='block_4')
-        self.sc4 = layers.Conv2D(df_dim * 8, (1, 1), name='sc_4')
-        self.block5 = self._block(df_dim * 8, name='block_5')
-        self.sc5 = layers.Conv2D(df_dim * 8, (1, 1), name='sc_5')
-
-        self.fc = layers.Dense(1, name='fc')
-
-    def call(self, x, training=None):
-        fx = self.block1(x, training=training)
-        x = dsample(self.sc1(x)) + dsample(fx)
-        fx = self.block2(x, training=training)
-        x = dsample(self.sc2(x)) + dsample(fx)
-        fx = self.block3(x, training=training)
-        x = dsample(self.sc3(x)) + dsample(fx)
-        fx = self.block4(x, training=training)
-        x = dsample(self.sc4(x)) + dsample(fx)
-        fx = self.block5(x, training=training)
-        x = self.sc5(x) + dsample(fx)
-
-        x = tf.nn.relu(x)
-        x = tf.reduce_sum(x, axis=(1, 2))
-        x = self.fc(x)
-        return x
-
-    def _block(self, filters, name):
-        seq = tf.keras.Sequential([
-            layers.ReLU(),
-            layers.Conv2D(filters, (3, 3), (1, 1), 'same', name='conv_1'),
-            layers.ReLU(),
-            layers.Conv2D(filters, (3, 3), (1, 1), 'same', name='conv_2')
-        ],
-                                  name=name)
-        return seq
-
-
 class BigGAN64(tf.keras.Model):
     def __init__(self, latent_dim, num_classes, generator, discriminator,
                  **kwargs):
@@ -365,7 +211,7 @@ class BigGAN64(tf.keras.Model):
 
 if __name__ == '__main__':
     batch_size = 1
-    z_dim = 100
+    z_dim = 120
     image_size = (64, 64)
     num_classes = 10
     base_dim = 64
